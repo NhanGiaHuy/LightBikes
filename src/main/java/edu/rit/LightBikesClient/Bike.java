0145e9a9bf0e43b91a0adbc0a3046fcc154ae54b
Justin W. Flory <git@j-f.co>
2016-05-01 19:28:41 -0400

    Revert "Comment out the broken array grids @FelAprile"
    This reverts commit d8b01cd9ff7f4247887fc127b00f77d05f319904.

--- src/main/java/edu/rit/LightBikesClient/Bike.java
+++ src/main/java/edu/rit/LightBikesClient/Bike.java
@@ -9,6 +9,8 @@
 package edu.rit.LightBikesClient;
 
 import java.util.concurrent.TimeUnit;
+import com.sun.org.apache.xerces.internal.impl.xpath.XPath;
+import sun.net.www.content.text.plain;
 
 /**
  * Bike.java
@@ -50,7 +52,7 @@ public class Bike {
 
 	public void turnWest(){
 		while(true){
-			//grid[xPosition-1][yPosition] = player;
+			grid[xPosition-1][yPosition] = player;
 			xPosition = xPosition - 1;
 			try{
 				TimeUnit.MILLISECONDS.sleep(DELAY_IN_MILLS);
@@ -63,7 +65,7 @@ public class Bike {
 
 	public void turnEast(){
 		while(true){
-			//grid[xPosition+1][yPosition] = player;
+			grid[xPosition+1][yPosition] = player;
 			xPosition = xPosition + 1;
 			try{
 				TimeUnit.MILLISECONDS.sleep(DELAY_IN_MILLS);
@@ -76,7 +78,7 @@ public class Bike {
 
 	public void turnSouth(){
 		while(true){
-			//grid[xPosition][yPosition+1] = player;
+			grid[xPosition][yPosition+1] = player;
 			yPosition = yPosition + 1;
 			try{
 				TimeUnit.MILLISECONDS.sleep(DELAY_IN_MILLS);
@@ -89,7 +91,7 @@ public class Bike {
 
 	public void turnNorth(){
 		while(true){
-			//grid[xPosition][yPosition-1] = player;
+			grid[xPosition][yPosition-1] = player;
 			yPosition = yPosition - 1;
 			try{
 				TimeUnit.MILLISECONDS.sleep(DELAY_IN_MILLS);
