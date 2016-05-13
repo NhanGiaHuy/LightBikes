ff7742c1fb6ed603530202e9b833d58afb851a5b
mxj9492 <mxj9492@rit.edu>
2016-05-12 20:37:47 -0400

    LightBikes javadocs

--- src/main/java/edu/rit/LightBikesClient/LightBikes.java
+++ src/main/java/edu/rit/LightBikesClient/LightBikes.java
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2016 Felice Aprile, Justin W. Flory, Malcolm Jones, Timothy Endersby
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
+* Copyright (c) 2016 Felice Aprile, Justin W. Flory, Malcolm Jones, Timothy Endersby
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 
 package edu.rit.LightBikesClient;
 
@@ -13,30 +13,30 @@ import java.awt.*;
 import java.awt.event.*;
 
 /**
- * LightBikes.java <p>
- *
- * Assignment: Final Project <p>
- * Class: Rochester Institute of Technology, ISTE-121.01, 2155 <p>
- * Professor: Michael Floeser <p>
- *
- * This class is the main class for the LightBikes game created for our team's
- * final project in ISTE-121. The game is modeled after the TRON-esque light
- * bikes in the movie and in the arcade. Players control a bike that leaves a
- * "wall of light" behind them as they travel across the game grid. They face
- * an opponent who is also leaving a wall of light behind them. If one player
- * hits the wall of light, they lose and the game ends.
- * <p>
- *
- * This class is the main file and is executed as the game client. <p>
- *
- * This class is responsible for the GUI
- *
- * @author Felice Aprile
- * @author Justin W. Flory
- * @author Malcolm Jones
- * @author Timothy Endersby
- * @version 2016.04.11.v1
- */
+* LightBikes.java <p>
+*
+* Assignment: Final Project <p>
+* Class: Rochester Institute of Technology, ISTE-121.01, 2155 <p>
+* Professor: Michael Floeser <p>
+*
+* This class is the main class for the LightBikes game created for our team's
+* final project in ISTE-121. The game is modeled after the TRON-esque light
+* bikes in the movie and in the arcade. Players control a bike that leaves a
+* "wall of light" behind them as they travel across the game grid. They face
+* an opponent who is also leaving a wall of light behind them. If one player
+* hits the wall of light, they lose and the game ends.
+* <p>
+*
+* This class is the main file and is executed as the game client. <p>
+*
+* This class is responsible for the GUI
+*
+* @author Felice Aprile
+* @author Justin W. Flory
+* @author Malcolm Jones
+* @author Timothy Endersby
+* @version 2016.04.11.v1
+*/
 public class LightBikes extends JFrame implements KeyListener, MouseListener {
 
     //These are test items, I'm not sure what we need here yet
@@ -48,27 +48,27 @@ public class LightBikes extends JFrame implements KeyListener, MouseListener {
     private String username;
 
     /**
-     * Starts the game
-     * @param args Nothing
-     */
+    * Starts the game
+    * @param args Nothing
+    */
     public static void main(String[] args){
         new LightBikes();
     }
 
     /**
-     * Builds the GUI
-     * Starts game logic classes
-     */
+    * Builds the GUI
+    * Starts game logic classes
+    */
     public LightBikes(){
         //Add JMenu bar
         JMenuBar menuBar = new JMenuBar();
-            JMenu jmFile = new JMenu("File");
-                jmiExit = new JMenuItem("Exit");
-            jmFile.add(jmiExit);
+        JMenu jmFile = new JMenu("File");
+        jmiExit = new JMenuItem("Exit");
+        jmFile.add(jmiExit);
         menuBar.add(jmFile);
-            JMenu jmHelp = new JMenu("Help");
-                jmiAbout = new JMenuItem("About");
-            jmHelp.add(jmiAbout);
+        JMenu jmHelp = new JMenu("Help");
+        jmiAbout = new JMenuItem("About");
+        jmHelp.add(jmiAbout);
         menuBar.add(jmHelp);
         add(menuBar, BorderLayout.NORTH);
 
@@ -122,8 +122,8 @@ public class LightBikes extends JFrame implements KeyListener, MouseListener {
     }
 
     /**
-    * Key listeners for controlling your bike
-     */
+    * Key listeners to listen for user input to control the bike.
+    */
     @Override
     public void keyPressed(KeyEvent ke){
 
@@ -142,29 +142,64 @@ public class LightBikes extends JFrame implements KeyListener, MouseListener {
     }
 
     /**
-    * Gets focus back to the grid for keylistener
-     */
+    * Returns the focus of the GUI to the grid.
+    */
     @Override
     public void mouseEntered(MouseEvent e) {
         gameGrid.requestFocus();
     }
 
-    //Unused methods
+    /**
+    * Unused.
+    * @param ke KeyEvent
+    */
     @Override
-    public void keyReleased(KeyEvent ke){}
+    public void keyReleased(KeyEvent ke){
 
-    @Override
-    public void keyTyped(KeyEvent ke){}
+    }
 
+    /**
+    * Unused.
+    * @param ke KeyEvent
+    */
     @Override
-    public void mouseClicked(MouseEvent e) {}
+    public void keyTyped(KeyEvent ke){
 
-    @Override
-    public void mousePressed(MouseEvent e) {}
+    }
 
+    /**
+    * Unused.
+    * @param ke KeyEvent
+    */
     @Override
-    public void mouseReleased(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {
 
+    }
+
+    /**
+     * Unused.
+     * @param e MouseEvent
+     */
     @Override
-    public void mouseExited(MouseEvent e) {}
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    /**
+     * Unused.
+     * @param e MouseEvent
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    /**
+     * Unused.
+     * @param e MouseEvent
+     */
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
